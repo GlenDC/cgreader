ff1282fb64534f02e49a10af66b8ab8efb1bb32a
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-05-17 13:44:11 +0200

    First, super basic version of cgreader

---
+++ cgreader.go
@@ -0,0 +1,14 @@
+package cgreader
+
+import (
+    "fmt"
+    //"io"
+    "io/ioutil"
+)
+
+func RunProgram(input string) {
+    file, err := ioutil.ReadFile(input)
+    if err == nil {
+        fmt.Print(file)
+    }
+}
