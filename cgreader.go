1dc71447614b27b8ee467d6f9cac246821f7f2ad
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-06-01 18:58:23 +0200

    Adding map rendering functionality

--- cgreader.go
+++ cgreader.go
@@ -173,3 +173,27 @@ func RunTargetProgram(in string, trace bool, program TargetProgram) {
 		}
 	}
 }
+
+type MapObject interface {
+	GetMapCoordinates() string // returns string in format x;y
+	GetMapIcon() string        // return 1 character string
+}
+
+func DrawMap(width, height int, background string, objects ...MapObject) {
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			position := fmt.Sprintf("%d;%d", x, y)
+			c := background
+			for _, object := range objects {
+				pos := object.GetMapCoordinates()
+				if pos == position {
+					c = object.GetMapIcon()
+					break
+				}
+			}
+			fmt.Printf("%s  ", c)
+		}
+		fmt.Println("")
+	}
+	fmt.Println("")
+}
