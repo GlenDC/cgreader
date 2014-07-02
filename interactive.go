9d9983fa1e00358058ce50858b92eb0588e27ddc
Glen De Cauwsemaecker <decauwsemaecker.glen@gmail.com>
2014-07-02 22:06:09 +0200

    defining the different interactive levels

---
+++ interactive.go
@@ -0,0 +1,25 @@
+package cgreader
+
+const (
+	PT_RAGNAROK        = "ragnarok"
+	PT_RAGNAROK_GIANTS = "ragnarok_giants"
+
+	PT_KIRK = "kirk"
+
+	PT_SKYNET         = "skynet"
+	PT_SKYNET_FINAL_1 = "skynet_final_1"
+	PT_SKYNET_FINAL_2 = "skynet_final_2"
+
+	PT_MARS_LANDER_1 = "mars_lander_1"
+	PT_MARS_LANDER_2 = "mars_lander_2"
+	PT_MARS_LANDER_3 = "mars_lander_3"
+
+	PT_INDIANA_1 = "indiana_1"
+	PT_INDIANA_2 = "indiana_2"
+	PT_INDIANA_3 = "indiana_3"
+
+	PT_KIRK_LABYRINTH = "labyrinth"
+
+	PT_SHADOW_KNIGHT_1 = "shadow_knight_1"
+	PT_SHADOW_KNIGHT_2 = "shadow_knight_2"
+)
